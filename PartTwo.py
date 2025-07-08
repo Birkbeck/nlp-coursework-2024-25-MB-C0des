92f678dbe2ff90cd582ae01379ff653c91cd2c12
MB-C0des <mobolaji.otunuga@hotmail.com>
2025-07-08 18:50:46 +0100

    read in csv

--- PartTwo.py
+++ PartTwo.py
@@ -1,13 +1,22 @@
 import pandas as pd
-from pathlib import Path
+from pathlib import Path # to use Path for file paths
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def load_and_filter_data(path=Path.cwd() / "hansard40000.csv"):
-    # 1. Load
+
     df = pd.read_csv(path)
+    print(df.head)
+    return
 
     
 # Read in the hansard4000.csv into a pandas dataframe
 # df = pd.read_csv()
 
-print(df.describe())
+
+if __name__ == "__main__":
+    """
+    uncomment the following lines to run the functions once you have completed them
+    """
+    load_and_filter_data()
+    
