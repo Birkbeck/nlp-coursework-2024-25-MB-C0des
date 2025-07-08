00f4f90715b3d4cf195f159e0d74bc2391f95f2a
MB-C0des <mobolaji.otunuga@hotmail.com>
2025-07-08 19:01:13 +0100

    clean csv data

--- PartTwo.py
+++ PartTwo.py
@@ -5,13 +5,37 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 def load_and_filter_data(path=Path.cwd() / "hansard40000.csv"):
 
-    df = pd.read_csv(path)
-    print(df.head)
-    return
+    df = pd.read_csv(path) # Read in the hansard4000.csv into a pandas dataframe
+    print(df.head) # df = pd.read_csv()
 
+    print(df.describe())
     
-# Read in the hansard4000.csv into a pandas dataframe
-# df = pd.read_csv()
+    # Rename the "Labour (Co-op)" value to "Labour" in the party column
+    df.replace('Labour (Co-op)', 'Labour', inplace=True)
+    print(df['party'].value_counts())
+
+    # Remove any rows where the party column isn't one of the top four (not including speaker)
+    df = df[df.party != "Speaker"]
+
+    top_parties = df['party'].value_counts().nlargest(4).index
+    df = df[df['party'].isin(top_parties)]
+
+    #df.drop(party=["Speaker"],inplace=True)
+    print(df['party'].value_counts())
+
+    # Remove any rows where the 'speech_class' column is NOT "Speech"
+    df = df[df.speech_class == "Speech"]
+    print(df['speech_class'].value_counts())
+
+    # Remove rows where text in the 'speech' column is <1000 characters
+
+    df = df[df.speech.str.len() >= 1000]
+
+    print(df.shape)
+
+    return df
+
+
 
 
 if __name__ == "__main__":
